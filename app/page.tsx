afc9951651ce6042dcb753b49e56d4e197d583f4
hiimstevejin <jingle5695@gmail.com>
2026-04-18 17:51:18 -0400

    chore: little fix idk when but some time in the past

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import { Button } from "@/components/ui/button"
+import Articles from "@/components/articles/articles"
+import KeywordTreemapWrapper from "@/components/keyword-treemap/keyword-treemap-wrapper"
 
-export default function Page() {
+export default function Home() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+      <KeywordTreemapWrapper />
+      <Articles />
+    </main>
   )
 }
